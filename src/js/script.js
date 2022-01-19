1928558a806a7da5e48eac4f8461a674c7428f3d
Василий Агафонов <new.smile321@bk.ru>
2022-01-19 17:26:54 +0300

    newerror

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+// var name = "Ivan";
+// let number = 7;
+// const pi = 3.14;
+
+// let leftborderwidth = 200;
+
+
+// number 
+// string 
+// true/false
+// null 
+// undefined 
+// let obj = {   
+//     name: 'apple',
+//     color: 'green'
+//     weight: 200
+// }
+// Symbol
+
+// alert(qwe)
+// console.log("sdfsdf")
+
+// if (2*4 == 8*1) {
+//     console.log("verno")
+// } else {
+//     console.log('error')
+// }
+
+// let answer = confirm("ва есть 18?")
+// if (answer) {
+//     console.log("prtxodi")
+// } else {
+//     console.log("efw")
+// }
+
+let aswer = confirm("Если лох, то нажми ок")
+if (aswer == true) {
+    alert("Ты лох")
+} else {
+    alert("ты ровный чел")
+}
